541c550afd29f69d5fae25772a6c6c376ce2d171
JuanCalel <jcalelr3@miumg.edu.gt>
2024-03-23 15:51:10 -0600

    Se agrego un nuevo componente

--- ComponentePrueba.js
+++ ComponentePrueba.js
@@ -5,6 +5,7 @@ const ComponentePrueba = () => {
     <div>
       <h1>Componente de prueba</h1>
       <p>Este es un componente de prueba para React.</p>
+      <p>Este es un componente de prueba para React.</p>
     </div>
   );
 };
