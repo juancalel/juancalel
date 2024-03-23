b57d9c57b17dc03ce1b20d292ce6f58111261b13
JuanCalel <jcalelr3@miumg.edu.gt>
2024-03-23 15:25:09 -0600

    Se agrego un componente de prueba para react

---
+++ ComponentePrueba.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComponentePrueba = () => {
+  return (
+    <div>
+      <h1>Componente de prueba</h1>
+      <p>Este es un componente de prueba para React.</p>
+    </div>
+  );
+};
+
+export default ComponentePrueba;
---
+++ proyecto-prueba/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentePrueba from './components/ComponentePrueba/ComponentePrueba';
+
+const App = () => {
+  return (
+    <div>
+      <ComponentePrueba />
+    </div>
+  );
+};
+
+export default App;
